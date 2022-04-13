03eb7dead75bbe0b9c3c4d8419d4475d9d34c250
parikshya098 <pbhandari1@ualr.edu>
2022-04-13 15:38:53 +0000

    unit 10

--- 10.02 Retail Item.py
+++ 10.02 Retail Item.py
@@ -1,19 +1,20 @@
 class RetailItem: 
-    def __init__(self, Description, UnitOnHand,Price): 
+    def __init__(self, Description='', UnitOnHand=0, Price=0): 
         self.Description = Description 
         self.UnitOnHand = UnitOnHand 
         self.Price = Price 
     def InventoryValue(self): 
         return (self.UnitOnHand * self.Price) 
-    def main():
-        with open("10.02 Inventory.txt") as file:
-            for line in file:
-                description, unit_on_hand, price = line.split(",") 
-                retail_item = RetailItem(description, int(unit_on_hand), float(price))
-                print("Description:", retail_item.Description) 
-                print("Units On Hand:", retail_item.UnitOnHand) 
-                print("Price:", retail_item.Price) 
-                print("Inventory Value:", retail_item.InventoryValue()) 
-                    
-                print() # Add an extra blank line for readability
-                main()
+inventoryfile = open("10.02 Inventory.txt","r")
+inventorylist = []
+x = inventoryfile.readline()
+while x !="":
+    description, unit_on_hand, price = x.split(",")
+    item = RetailItem(description, int(unit_on_hand), float(price))
+    inventorylist.append(item)
+    x = inventoryfile.readline()
+print("{:>20s}{:>20s}{:>20s}{:>20s}".format("Description","Units On Hand","Price","Inventory Value"))
+for i in range(len(inventorylist)):
+    print("{:>20s}{:>20d}{:>20.2f}{:>20.2f}".format(inventorylist[i].Description,inventorylist[i].UnitOnHand,inventorylist[i].Price,inventorylist[i].InventoryValue()))
+
+print() # Add an extra blank line for readability
