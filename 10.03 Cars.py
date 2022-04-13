43edaf235b8591d942553c660248602012c5e89b
parikshya098 <pbhandari1@ualr.edu>
2022-04-13 05:47:44 +0000

    unit 10

---
+++ 10.03 Cars.py
@@ -0,0 +1,11 @@
+def __init__(self, make, year_model, speed):
+    self.__make = make
+    self.__year_model = year_model
+    self.__speed = 0
+
+def accelerate(self):
+    self.__speed += 5
+def brake(self):
+    self.__speed -= 5
+
+filename = open("10.03 Cars.txt", "r")
