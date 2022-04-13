1f1d3d3078f171195ed283aa3e1b4082ff22a235
parikshya098 <pbhandari1@ualr.edu>
2022-04-13 15:42:34 +0000

    Unit 10

--- 10.03 Cars.py
+++ 10.03 Cars.py
@@ -1,11 +1,34 @@
-def __init__(self, make, year_model, speed):
-    self.__make = make
-    self.__year_model = year_model
-    self.__speed = 0
+import csv
 
-def accelerate(self):
-    self.__speed += 5
-def brake(self):
-    self.__speed -= 5
+class Car:
+    def __init__(self,year,make,speed=0):
+        self.year = year
+        self.make = make
+        self.speed = speed
 
-filename = open("10.03 Cars.txt", "r")
+    def accelarate(self,value):
+        self.speed = self.speed + value
+
+    def brake(self,value):
+        self.speed = self.speed + value
+
+if __name__ == "__main__":
+    flag = True
+    changes = []
+    with open('10.03 Cars.txt') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if flag:
+                car = Car(year=row[0],make=(row[1]).strip())
+                flag = False
+                continue
+            changes.append(int(row[0]))
+    print('Make: ',car.make)
+    print('Year: ',car.year)
+    print("\nChange\tSpeed")
+    for change in changes:
+        if change>0:
+            car.accelarate(change)
+        else:
+            car.brake(change)
+        print(f"{change}\t{car.speed}")
