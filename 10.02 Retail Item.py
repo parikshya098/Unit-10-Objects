94a1cb8d8c00dc5377fec5371bcbb88f750198f7
parikshya098 <pbhandari1@ualr.edu>
2022-04-13 04:47:26 +0000

    Unit 10

--- 10.02 Retail Item.py
+++ 10.02 Retail Item.py
@@ -8,17 +8,15 @@ class RetailItem:
         return self.UnitsOnHand * self.Price  
 
     def main():
-        with open("10.02 Inventory.txt") as file:
-            for line in file:
-                description, units_on_hand, price = line.split(",") 
-                retail_item = RetailItem(description, int(units_on_hand), float(price))
-
-print("Description:", retail_item.Description) 
-
-print("Units On Hand:", retail_item.UnitsOnHand) 
-print("Price:", retail_item.Price) 
-print("Inventory Value:", retail_item. InventoryValue()) 
-
-print() # Add an extra blank line for readability
-
-main()
+        file1 = '10.02 Inventory.txt'
+        
+file1 = open("10.02 Inventory.txt", 'r')
+for line in file1:
+    description, units_on_hand, price = line.split(",") 
+    retail_item = RetailItem(description, int(units_on_hand), float(price))
+    print("Description:", retail_item.Description) 
+    print("Units On Hand:", retail_item.UnitsOnHand) 
+    print("Price:", retail_item.Price)
+    print("Inventory Value:", retail_item. InventoryValue()) 
+    
+    print() # Add an extra blank line for readability
